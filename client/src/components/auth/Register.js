ccf3673d830f3873ca278dd5defdeab03d282301
Amanda <amandabaydar@gmail.com>
2021-10-26 14:47:17 -0400

    set up login route/ add code comment to send data w/o redux

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,7 @@
 import React, { Fragment, useState } from 'react'
+import { Link } from 'react-router-dom'
+
+// import axios from 'axios'
 
 const Register = () => {
 
@@ -19,7 +22,29 @@ const Register = () => {
         if(password !== password2) {
             console.log('Passwords do not match')
         } else {
-            console.log(formData)
+            console.log('Success')
+        // example code to send to backend without using redux:
+        // (make sure to add async before 'e' in order to use async await)
+        //     const newUser = {
+        //         name,
+        //         email,
+        //         password
+        //     }
+
+        //     try{
+        //         const config = {
+        //             headers: {
+        //                 'Content-Type': 'application/json'
+        //             }
+        //         }
+
+        //         const body = JSON.stringify(newUser)
+
+        //         const res = await axios.post('/api/users', body, config)
+        //         console.log(res.data)
+        //     }catch(err){
+        //         console.error(err.response.data)
+        //     }
         }
     }
 
@@ -33,11 +58,9 @@ const Register = () => {
                 </div>
                 <div className="form-group">
                 <input type="email" placeholder="Email Address" name="email" value={email} onChange={e => onChange(e)} required />
-                <small className="form-text"
-                    >This site uses Gravatar so if you want a profile image, use a
-                    Gravatar email</small
-                >
                 </div>
+                <small class="form-text">This site uses Gravatar so if you want a profile image, use a
+                    Gravatar email</small>
                 <div className="form-group">
                 <input
                     type="password"
@@ -59,7 +82,7 @@ const Register = () => {
                 <input type="submit" className="btn btn-primary" value="Register" />
             </form>
             <p className="my-1">
-                Already have an account? <a href="login.html">Sign In</a>
+                Already have an account? <Link to="/login">Sign In</Link>
             </p>
         </Fragment>
     )
