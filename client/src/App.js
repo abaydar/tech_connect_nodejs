eddb5d1106f3f6bc34f357545fb96f871b1bd6e2
Amanda <amandabaydar@gmail.com>
2021-11-01 15:28:48 -0400

    create profile form and handle state on front end

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import { loadUser } from './actions/auth';
 import setAuthToken from './utils/setAuthToken';
 import Dashboard from './components/dashboard/Dashboard';
 import PrivateRoute from './components/routing/PrivateRoute';
+import CreateProfile from './components/profile-forms/CreateProfile';
 import './App.css';
 
 if(localStorage.token){
@@ -34,6 +35,7 @@ const App = () => {
                         <Route exact path='/register' component={Register}/>
                         <Route exact path='/login' component={Login}/>
                         <PrivateRoute exact path='/dashboard' component={Dashboard}/>
+                        <PrivateRoute exact path='/create-profile' component={CreateProfile}/>
                     </Switch>
                 </section>
             </Fragment>
