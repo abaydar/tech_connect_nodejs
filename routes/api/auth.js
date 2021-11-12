aafbd061b6ba30cfeae64003eb1f896799b4074c
Amanda <amandabaydar@gmail.com>
2021-11-12 13:24:52 -0500

    remove comments

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -8,14 +8,13 @@ const auth = require('../../middleware/auth')
 const User = require('../../models/User')
 
 // @route   GET api/auth
-// @desc    Test route
+// @desc    Get user by token
 // @access  Public
 router.get('/', auth, async (req, res) => {
     try {
         const user = await User.findById(req.user.id).select('-password')
         res.json(user)
     } catch(err){
-        console.error(err.message)
         res.status(500).send('Server error')
     }
 })
@@ -70,7 +69,6 @@ async (req, res) => {
         )
 
     } catch(err){
-        console.error(err.message)
         res.status(500).send('Server error')
     }
     
