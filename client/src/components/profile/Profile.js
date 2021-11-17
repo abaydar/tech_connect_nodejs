e19f00e6f688b9a486ebd1fccd8a21ad148bacb0
Amanda <amandabaydar@gmail.com>
2021-11-17 12:22:32 -0500

    set up profile top and about section

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -3,7 +3,9 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import Spinner from '../layout/Spinner'
+import ProfileAbout from './ProfileAbout'
 import { getProfileById } from '../../actions/profile'
+import ProfileTop from './ProfileTop'
 
 
 const Profile = ({ getProfileById, profile: { profile, loading }, auth, match }) => {
@@ -19,6 +21,10 @@ const Profile = ({ getProfileById, profile: { profile, loading }, auth, match })
                         Back to Profiles
                     </Link>
                     { auth.isAuthenticated && auth.loading === false && auth.user._id === profile.user._id && (<Link to='/edit-profile' className="btn btn-dark">Edit Profile</Link>)}
+                    <div class="profile-grid my-1">
+                        <ProfileTop profile={profile}/>
+                        <ProfileAbout profile={profile}/>
+                    </div>
                 </Fragment>}
         </Fragment>
     )
