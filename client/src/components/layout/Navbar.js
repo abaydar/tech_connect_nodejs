4876d12373b89653c4d66bda17ec5fd7f4d0b5da
Amanda <amandabaydar@gmail.com>
2021-11-19 14:54:05 -0500

    set up posts reducer, action, and component

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -10,6 +10,7 @@ const Navbar = ({ logout, auth:{ isAuthenticated, loading } }) => {
       <li><Link to="/dashboard">
       <i className="fas fa-user"></i>{' '}
       <span className="hide-sm">Dashboard</span></Link></li>
+      <li><Link to="/posts">Posts</Link></li>
       <li><Link to="/profiles">Developers</Link></li>
       <li><a onClick={logout} href="#!">
         <i className="fas fa-sign-out-alt"></i>{' '}
