2ce4b7c1074da7884a3d4b6f36188a18330f050a
Amanda <amandabaydar@gmail.com>
2021-12-07 14:59:41 -0500

    finish setting up post page

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -3,17 +3,18 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import Spinner from '../layout/Spinner'
 import { getPost } from '../../actions/post'
+import PostItem from '../posts/PostItem'
+import { Link } from 'react-router-dom'
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
     useEffect(() => {
         getPost(match.params.id)
     },[getPost])
     
-    return (
-        <div>
-            post
-        </div>
-    )
+    return loading || post === null ? <Spinner /> : <Fragment>
+        <Link to='/posts' className='btn'>Back to Posts</Link>
+        <PostItem post={post} showActions={false} />
+    </Fragment>
 }
 
 Post.propTypes = {
